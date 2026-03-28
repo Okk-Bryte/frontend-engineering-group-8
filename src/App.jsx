420344232d5f60a91c4682dca2ae79e79ac8e547
Kosii Okeke <bryteokk@gmail.com>
2026-03-28 02:51:54 +0100

    Local changes before pulling main

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import Header       from './components/Header'
-import Hero          from './components/Hero'
-import MediaSection  from './components/MediaSection'
-import PlanetGallery from './components/PlanetGallery'
-import PlanetTable   from './components/PlanetTable'
-import ContactForm   from './components/ContactForm'
-import Footer        from './components/Footer'
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import VideoSection from "./components/VideoSection/VideoSection";
+import Planets from "./components/Planets/Planets";
+import Table from "./components/Table/Table"
+import ContactForm from "./components/ContactForm/ContactForm";
+import Footer from "./components/Footer/Footer";
+
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
-      <MediaSection />
-      <PlanetGallery />
-      <PlanetTable />
+      <VideoSection />
+      <Planets />
+      <Table />
       <ContactForm />
       <Footer />
     </>
