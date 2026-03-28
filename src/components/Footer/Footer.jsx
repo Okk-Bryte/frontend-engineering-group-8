bba49bdf9040f166dfa93e28f37bf3e4f445ac43
Kosii Okeke <104731843+Okk-Bryte@users.noreply.github.com>
2026-03-28 09:40:56 +0100

    Update footer link text for Frontend Engineering Group 8

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="my-footer-bottom">
         <div className="my-footer-copy">
           <p>©2026 Design by <a href="https://github.com/Anurella" target="_blank" rel="noreferrer">Amaka</a> & <a href="https://www.linkedin.com/in/ifeomaokocha" target="_blank" rel="noreferrer">Ifeoma</a> A.</p>
-          <p>Built by <a href="https://github.com/Okk-Bryte/frontend-engineering-group-8" target="_blank" rel="noreferrer">Frontend Engineering Group 8</a>. All rights reserved</p>
+          <p>Built by <a href="https://github.com/Okk-Bryte/frontend-engineering-group-8" target="_blank" rel="noreferrer">Frontend-Engineering-Group-8</a>. All rights reserved</p>
         </div>
         <a href="https://tsacademyonline.com/" target="_blank" rel="noreferrer" className="my-footer-ts-link">TS Academy</a>
       </div>
