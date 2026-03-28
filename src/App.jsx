29f3c8516f96561d4ef79350df08f8ba7fd1b651
Kosii Okeke <104731843+Okk-Bryte@users.noreply.github.com>
2026-03-28 05:06:27 +0100

    Comment out ContactForm component
    Comment out the ContactForm component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero/Hero";
 import VideoSection from "./components/VideoSection/VideoSection";
 import Planets from "./components/Planets/Planets";
 import Table from "./components/Table/Table"
-import ContactForm from "./components/ContactForm/ContactForm";
+// import ContactForm from "./components/ContactForm/ContactForm";
 import Footer from "./components/Footer/Footer";
 
 
@@ -15,7 +15,6 @@ export default function App() {
       <VideoSection />
       <Planets />
       <Table />
-      <ContactForm />
       <Footer />
     </>
   )
